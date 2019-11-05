82ebb972f25a5968d239e129925041b50bc16c0a
Ganga Lal Chapain <gangalalchapain@gmail.com>
2019-11-05 05:55:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  result = [];
+  for(let i = 0; i < musicians.length; i++){
+    result.push(musicians + " plays " + instruments);
+  }
+}
