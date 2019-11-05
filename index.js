f4501c58e4d17d74689f63c67498da0b77033fca
Ganga Lal Chapain <gangalalchapain@gmail.com>
2019-11-05 05:56:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ function theBeatlesPlay(musicians, instruments){
   result = [];
   for(let i = 0; i < musicians.length; i++){
     result.push(musicians + " plays " + instruments);
-  }
+  }return result;
 }
+
